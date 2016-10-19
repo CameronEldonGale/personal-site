1a1efd9958d1eca1211a028581a75cd2af9fb939
Cameron Gale <cameroneldongale@gmail.com>
2016-10-19 13:14:14 -0600

    cleaning up

---
+++ js/typing.js
@@ -0,0 +1,26 @@
+$(function(){
+  var content = ["Hi!", "My name is Cameron","I am a web developer from Utah", "Checkout my projects and resume"]
+    var i = 0;
+    function addLine(){
+      $('.typed-lines').append( '<p class="element">'+content[i-1]+'</p>')
+    }
+    var typeOnScreen = function (){
+
+      $(".line").typed({
+        strings: [content[i]],
+        showCursor: false,
+        typeSpeed: 0,
+        callback: function(){
+          console.log(i);
+          i++
+          if (i < content.length) {
+            setTimeout(addLine, 2000)
+            setTimeout(typeOnScreen, 2000)
+          }
+
+        }
+      })
+    }
+    setTimeout(typeOnScreen, 500)
+
+});
