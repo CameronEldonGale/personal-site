a916f4208b1351d02a0ac389bdce06f1635c42c1
Cameron Gale <cameroneldongale@gmail.com>
2016-10-19 10:42:14 -0600

    mobile

--- js/justVanilla.js
+++ js/justVanilla.js
@@ -27,6 +27,31 @@ window.addEventListener("load", function(event) {
       var body = document.getElementById("body").value;
       window.location.href = "mailto:cameroneldongale@gmail.com?subject=Hello&body="+body;
     }
+    function getBrowserInfo(){
+    	var ua = navigator.userAgent, tem,
+    	M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    	if(/trident/i.test(M[1]))
+    	{
+    		tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+    		return 'IE '+(tem[1] || '');
+    	}
+    	if(M[1]=== 'Chrome')
+    	{
+    		tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+    		if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    	}
+    	M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    	if((tem= ua.match(/version\/(\d+)/i))!= null)
+    		M.splice(1, 1, tem[1]);
+    	return M.join(' ');
+    }
+
+    var browserInfo = getBrowserInfo();
+    console.log(browserInfo.slice(0,6));
+    if (browserInfo.slice(0,6) !== "Chrome") {
+      location.href = 'https://www.google.com/chrome/browser/desktop/';
+    }
+
 });
 
 
